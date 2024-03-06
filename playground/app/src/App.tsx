53c54d43c968cd8b620f2c529490e51f8f74bd7b
Santhoshkumar <santhoshkumar9028@gmail.com>
2024-03-06 16:49:42 +0530

    Added playground app

---
+++ playground/app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Button } from '@ui-blocks/ds'
+
+function App() {
+  return (
+    <>
+      <Button color='red'>Red button</Button>
+    </>
+  )
+}
+
+export default App
