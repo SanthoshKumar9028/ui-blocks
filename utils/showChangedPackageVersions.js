73aa6f814bd98f0960821f764c795eeb4376481c
Santhoshkumar <santhoshkumar9028@gmail.com>
2024-03-19 19:36:13 +0530

    updated script file

--- utils/showChangedPackageVersions.js
+++ utils/showChangedPackageVersions.js
@@ -1,14 +1,9 @@
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
-const getPackageVersion = async (packageFilePath) => {
-  const { stderr, stdout } = await exec(`git log --format="%H" -n 2`);
-  if (stderr) throw stderr;
-
-  const commitIds = stdout.split("\n").filter(Boolean);
-
+const getPackageVersion = async (packageFilePath, ...commits) => {
   const commitChanges = await Promise.all(
-    commitIds.map((id) => exec(`git show ${id}:${packageFilePath}`))
+    commits.map((commit) => exec(`git show ${commit}:${packageFilePath}`))
   );
 
   return commitChanges.map((commitChange) => {
@@ -28,13 +23,13 @@ const packages = [{ path: "./packages/common/package.json" }];
 (async () => {
   const changedPackages = await Promise.all(
     packages.map(({ path }) => {
-      return getPackageVersion(path);
+      return getPackageVersion(path, "HEAD", "HEAD~1");
     })
   );
 
   console.log(
     `changed_packages=${changedPackages
-      .filter(([cur, prv]) => cur.version != prv?.version)
+      .filter(([cur, prv]) => cur.version != prv.version)
       .map(([cur]) => cur.name)
       .join()}`
   );
